5fd86967793ca90630482d6cdc98defbb9002dff
MovingCairn <282053639+MovingCairn@users.noreply.github.com>
2026-05-22 03:59:21 -0400

    feat(frames): add icon/model portrait recipes and asset pipeline
    - ExampleFrameIconPortrait: PortraitFrameTemplate with custom icon texture
    - ExampleFrameModelPortrait: DressUpModel frame showing player model
    - dev/assets.sh: converts docs/**/*.svg to PNG or TGA (flipped for WoW)

---
+++ docs/frames/Addons/ExampleFrameIconPortrait_Vertex/ExampleFrameIconPortrait.lua
@@ -0,0 +1,3 @@
+ExampleFrameIconPortrait:SetTitle("Example Icon Portrait")
+ExampleFrameIconPortrait:SetPortraitToAsset("Interface\\AddOns\\ExampleFrameIconPortrait_Vertex\\vertex-icon")
+ExampleFrameIconPortrait:SetPortraitTextureSizeAndOffset(64, -6, 10)
---
+++ docs/frames/Addons/ExampleFrameModelPortrait_Vertex/_harness.lua
@@ -0,0 +1,24 @@
+-- HARNESS: Not part of the recipe. Registers /ev5 so you can toggle the frame
+-- in-game to see the example. Safe to delete when building your own addon.
+
+VertexExamples = VertexExamples or {}
+VertexExamples.ExampleFrameModelPortrait = ExampleFrameModelPortrait
+
+SLASH_EXAMPLEFRAMEMODELPORTRAIT1 = "/ev5"
+SlashCmdList["EXAMPLEFRAMEMODELPORTRAIT"] = function()
+    local f = ExampleFrameModelPortrait
+    if f:IsShown() then
+        f:Hide()
+    else
+        f.Model:SetUnit("player")
+        f.Model:SetCamera(0)
+        f:Show()
+    end
+end
+
+SLASH_VERTEXEXAMPLESOFF1 = "/evoff"
+SlashCmdList["VERTEXEXAMPLESOFF"] = function()
+    for _, f in pairs(VertexExamples) do
+        f:Hide()
+    end
+end
